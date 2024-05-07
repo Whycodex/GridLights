bfd4025ebdfc4934201487c88b14560ec2aa9e86
Whycodex <neerajgupta12300@gmail.com>
2024-05-07 23:58:33 +0530

    Updated cells

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
 
+function Cell(filled, onClick, isDisabled, label) {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
+}
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <div>
+        {[...Array(9)].map((value, index) => {
+          return value ? (
+            <Cell
+              key={index}
+              label={`Cell ${index}`}
+              filled={order.includes(index)}
+              onClick={() => activateCells(index)}
+              isDisabled={order.includes(index || isDeactivating)}
+            />
+          ) : (
+            <span />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
