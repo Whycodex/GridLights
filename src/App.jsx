9c02093f986be09c19f47ed79b77d5c64a522ad8
Whycodex <neerajgupta12300@gmail.com>
2024-05-09 23:33:08 +0530

    Completed grid lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 function Cell(filled, onClick, isDisabled, label) {
   return (
@@ -13,17 +14,45 @@ function Cell(filled, onClick, isDisabled, label) {
 }
 
 function App() {
-  
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+
   const config = [
     [1, 1, 1],
     [1, 0, 1],
     [1, 1, 1],
   ];
 
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const timer = setInterval(() => {
+      setOrder((originalOrder) => {
+        const newOrder = originalOrder.slice();
+        newOrder.pop();
+        if (newOrder.length == 0) {
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+        return newOrder;
+      });
+    }, 300);
+  };
+
+  const activateCells = (index) => {
+    const newOrder = [...order, index];
+    setOrder(newOrder);
+    if (newOrder.length === config.flat(1).filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
+
   return (
-    <div>
-      <div>
-        {[...Array(9)].map((value, index) => {
+    <div className="wrapper">
+      <div
+        className="grid"
+        style={{ gridTemplateColumns: `repeat(${config[0].length},1fr)` }}
+      >
+        {config.flat(1).map((value, index) => {
           return value ? (
             <Cell
               key={index}
